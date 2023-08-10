b309cff0593d4a0e32403d426e4dd51652a429ea
hareesdevkano <114575410+hareesdevkano@users.noreply.github.com>
2023-08-10 21:35:54 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+    from calculator_1 import add, subtract, multiply, divide
 
     nargs = len(sys.argv) - 1
     if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    op = sys.argv[2]
-    if op != '+' and op != '-' and op != '*' and op != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
+    op = sys.argv[2]
     b = int(sys.argv[3])
 
     if op == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        result = add(a, b)
     elif op == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        result = subtract(a, b)
     elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        result = multiply(a, b)
+    elif op == '/':
+        result = divide(a, b)
     else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, result))
